1b32e5fb371dc89801e8855413de1fa730795552
varun1760 <varunrao1760@gmail.com>
2025-09-14 18:13:38 +0530

    Project Started...

---
+++ src/test/java/com/rao/EasySchool/EasySchoolApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rao.EasySchool;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EasySchoolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
